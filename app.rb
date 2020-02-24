30a4b24be7bf4d349917586470a8ea2d970204ea
rhaynes3k <rhaynes3k@gmail.com>
2020-02-23 18:57:56 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, folks"
+  end
+
+  get '/name' do
+    "My name is Big Bad! "
+  end
+
+  get '/hometown' do
+    "My hometown is The Bad Lands. "
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Bad."
+  end
+
 end
