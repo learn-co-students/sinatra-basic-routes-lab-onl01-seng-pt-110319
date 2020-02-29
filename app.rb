59519cdaf07805c125ab6f9ab8fc623106338b08
melissa_holt <melissa.holt21@gmail.com>
2020-02-29 11:22:43 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Mel, The Coding Barista"
+    end
+
+    get '/hometown' do
+        "My hometown is Los Angeles, CA"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Defying Gravity"
+    end
 end
