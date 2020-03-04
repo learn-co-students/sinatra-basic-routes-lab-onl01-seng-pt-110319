9f4160d94bfb7cc4eae948c9279a88e5ead04174
Untitled1Unmastered1 <gabriel1994.gp@gmail.com>
2020-03-03 23:45:54 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+    get '/' do
+        "Hello, World!"
+      end
+      
+    get '/name' do
+        "My name is Gabe"
+    end
+
+    get '/hometown' do 
+        "My hometown is Havana"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is Fake Plastic Trees"
+    end
+end 
